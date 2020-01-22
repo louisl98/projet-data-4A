abc8c5d045ecef11b90efc1b5c2a7cf4dfa8ae6f
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-01-22 20:45:12 +0100

    testDeploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Hapi = require('@hapi/hapi');
 
 const init = async () => { 
     const server = Hapi.server({
-        port = 9001,
+        port = 3001,
         host: 'data-ratp-4a.herokuapp.com',
         routes: {
             cors: true
