da5ee820aa3e80f9c4946ceaca872ede8c6c18a4
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-01-22 20:34:31 +0100

    testDeploy

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const Hapi = require('@hapi/hapi');
 
 const init = async () => { 
     const server = Hapi.server({
-        port: 3001,
-        host: '0.0.0.0'
+        port: ~~process.env.PORT | 8000,
     });
     server.route(require('./routes/getClients'));
     await server.start();
