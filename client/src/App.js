e337343f56ca64b51c0c9cf5b09d1f246fafca06
Louis <louis-laugier@hotmail.fr>
2020-01-23 10:17:40 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+        <hr/>
+        <Clients/>
       </header>
-      <Clients/>
     </div>
   );
 }
