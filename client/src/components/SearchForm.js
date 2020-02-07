cae0a332a710d7672d7142fffd00eda134b95f80
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-02-07 22:26:55 +0100

    update

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -12,24 +12,17 @@ class SearchForm extends React.Component {
     this.limitChange = this.limitChange.bind(this);
     this.genreChange = this.genreChange.bind(this);
   }
-<<<<<<< HEAD
 
   limitChange(event) {
     this.setState({limit: event.target.value});
   }
 
-=======
-  limitChange(event) {
-    this.setState({limit: event.target.value});
-  }
->>>>>>> 52e6c697224a35e2eb2b5ba14d03a155ae687015
   genreChange(event) {
     this.setState({genre: event.target.value});
   }
 
   render() {
     return [
-<<<<<<< HEAD
       <form key="form" className="SearchForm">
         <label htmlFor="limit">Limite de résultats :</label>
         <input name="limit" className="Limit" type="text" onChange={this.limitChange}/>
@@ -38,16 +31,6 @@ class SearchForm extends React.Component {
           <option value="tous">Tous</option>
           <option value="homme">Homme</option>
           <option value="femme">Femme</option>
-=======
-      <form key="form" className="SearchForm" onSubmit={this.handleSubmit}>
-        <label htmlFor="limit">Limite de résultats :</label>
-        <input name="limit" type="text" value={this.state.limit} onChange={this.limitChange} />
-        <label htmlFor="genre">Genre :</label>
-        <select onChange={this.genreChange} name="genre">
-          <option value="tous">tous</option>
-          <option value="homme">homme</option>
-          <option value="femme">femme</option>
->>>>>>> 52e6c697224a35e2eb2b5ba14d03a155ae687015
         </select>
       </form>,
       <RatpClients key="results" limit={this.state.limit} genre={this.state.genre}/>
