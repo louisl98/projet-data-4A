6811513b5df0b697ceb54be8b55ba19eea0dc2cd
louisl98 <louis.laugier@etu-webschoolfactory.fr>
2020-02-11 20:33:06 +0100

    update

--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = require('knex')({
-    client: 'pg',
-    connection: 'postgres://student:gM4eyA2468DCsQeRBNvcHGYUt5@equipe-01.cqf0cfj75jd4.eu-west-3.rds.amazonaws.com:5432/postgres?ssl=disable'
-  });
